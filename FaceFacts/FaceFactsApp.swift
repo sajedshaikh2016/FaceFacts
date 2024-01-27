588ce84b9f40d7fd859c01dbc4cd248d14307f37
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-01-27 16:46:16 +0530

    Initial Commit

---
+++ FaceFacts/FaceFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceFactsApp.swift
+//  FaceFacts
+//
+//  Created by Sajed Shaikh on 27/01/24.
+//
+
+import SwiftUI
+
+@main
+struct FaceFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
