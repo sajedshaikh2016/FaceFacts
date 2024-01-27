74a31ede60174e7c4cd6ccd174bf96f46a7f9575
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-01-27 19:06:47 +0530

    Search functionality implemented

--- FaceFacts/ContentView.swift
+++ FaceFacts/ContentView.swift
@@ -11,24 +11,20 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @State private var path = [Person]()
-    @Query var people: [Person]
+    
+    @State private var searchText = ""
     
     var body: some View {
         NavigationStack(path: $path) {
-            List {
-                ForEach(people) { person in
-                    NavigationLink(value: person) {
-                        Text(person.name)
-                    }
+            PeopleView(searchString: searchText)
+                .navigationTitle("FaceFacts")
+                .navigationDestination(for: Person.self) { person in
+                    EditPersonIView(person: person)
                 }
-            }
-            .navigationTitle("FaceFacts")
-            .navigationDestination(for: Person.self) { person in
-                EditPersonIView(person: person)
-            }
-            .toolbar {
-                Button("Add Person", systemImage: "plus", action: addPerson)
-            }
+                .toolbar {
+                    Button("Add Person", systemImage: "plus", action: addPerson)
+                }
+                .searchable(text: $searchText)
         }
     }
     
---
+++ FaceFacts/PeopleView.swift
@@ -0,0 +1,48 @@
+//
+//  PeopleView.swift
+//  FaceFacts
+//
+//  Created by Sajed Shaikh on 27/01/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct PeopleView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var people: [Person]
+    
+    var body: some View {
+        List {
+            ForEach(people) { person in
+                NavigationLink(value: person) {
+                    Text(person.name)
+                }
+            }
+            .onDelete(perform: deletePeople)
+        }
+    }
+    
+    init(searchString: String = "") {
+        _people = Query(filter: #Predicate { person in
+            if searchString.isEmpty {
+                true
+            } else {
+                person.name.localizedStandardContains(searchString)
+                || person.emailAddress.localizedStandardContains(searchString)
+                || person.details.localizedStandardContains(searchString)
+            }
+        })
+    }
+    
+    func deletePeople(at offsets: IndexSet) {
+        for offset in offsets {
+            let person = people[offset]
+            modelContext.delete(person)
+        }
+    }
+}
+
+#Preview {
+    PeopleView()
+}
